9e538fc27d50c376eee75190fd998a3890a06aed
Anshp42 <159457093+Anshp42@users.noreply.github.com>
2024-09-12 10:29:28 -0400

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,33 @@
+//* Ansh patel
+//* Calculator 
+//*software design 
+//*9/12/24
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+    cout<<"what is a value you want: ";
+    cin >> num1;
+    cout<<"Enter another number: ";
+    cin >> num2;
+    int sum = num1+num2;
+    cout<<"value"<<sum;
+    int dif = num1-num2;
+    cout<<"Difference"<<dif;
+    int pro = num1*num2;
+    cout<<"product"<<pro;
+    int div = num1/num2;
+    cout<<"quotient"<<div;
+    int mod = num1%num2;
+    cout<<"Modulus"<<mod;
+    int inc = ++num1;
+    cout<<"increment"<<inc;
+    int dec = --num1;
+    cout<<"Decrement"<<dec;
+  int num3=15;
+  int total = num1/num2*num3;
+cout<<"Total"<<total<<endl;
+
+}
+
